8836aed8e39b0e1803acb7a8ce3cab0723624cf7
nanakoasakawa <whskk.u75@ezweb.ne.jp>
2022-04-26 23:21:00 +0900

    install laravel ui #5

--- attepj/routes/web.php
+++ attepj/routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', [AtteController::class, 'index']);
 //会員登録
 Route::get('/register', [App\Http\Controllers\AtteController::class, 'create']);
 Route::post('/register', [App\Http\Controllers\AtteController::class, 'store']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
